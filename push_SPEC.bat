7fac06ebee74f397d20d1991fe35c53952dd17a0
Michał Kucia <kucia.michal@gmail.com>
2026-05-19 11:40:46 +0200

    SPEC update 19.05.2026 11:40:46,41

--- push_SPEC.bat
+++ push_SPEC.bat
@@ -3,6 +3,6 @@ echo === SPEC - Push do GitHub ===
 cd /d "%USERPROFILE%\Downloads\SPEC"
 git add .
 git commit -m "SPEC update %date% %time%"
-git push origin master
+git push origin master:main
 echo === Gotowe! ===
 pause
